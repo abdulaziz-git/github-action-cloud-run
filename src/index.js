12d5af6ce9bdc1bacd5617dfa9334cef3f04737b
Abdul Aziz <aziz@blogdoang.com>
2023-05-10 10:40:16 +0700

    change text in index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,6 @@ app.listen(8888, () => {
 
 app.get("/", (req, res) => {
   res.send(
-    `This is simple nodejs website with CI/CD using GCP with fix`
+    `Hello world from GCP CI/CD with cloud run`
   );
 });
